db0a0b040477ded35a269cadbdfbbc16d07efbf3
André Alvim Ribeiro <andre2ar@outlook.com>
2020-09-13 16:31:16 -0300

    Toast icons

---
+++ src/components/ToastContainer/Toast/styles.ts
@@ -0,0 +1,71 @@
+import styled, { css } from "styled-components";
+
+interface ContainerProps {
+    type?: 'success' | 'error' | 'info';
+    hasDescription: boolean;
+}
+
+const toastTypeVariations = {
+    info: css`
+      background: #ebf8ff;
+      color: #3172b7;
+    `,
+    success: css`
+      background: #e6fffa;
+      color: #2e656a;
+    `,
+    error: css`
+      background: #fddede;
+      color: #c53030;
+    `,
+};
+
+export const Container = styled.div<ContainerProps>`
+  width: 36rem;
+  
+  position: relative;
+  padding: 1.6rem 3rem 1.6rem 1.6rem;
+  border-radius: 10px;
+  box-shadow: 2px 2px 8px rgba(0,0,0,0.2);
+  
+  display: flex;
+  
+  & + div {
+    margin-top: .8rem;
+  }
+  
+  ${props => toastTypeVariations[props.type || 'info']}
+  
+  > svg {
+    margin: 4px 12px 0 0;
+  }
+  
+  div {
+    flex: 1;
+    
+    p {
+      margin-top: .4rem;
+      font-size: 1.4rem;
+      opacity: 0.8;
+      line-height: 2rem;
+    }
+  }
+  
+  button {
+    position: absolute;
+    right: 1.6rem;
+    top: 1.9rem;
+    opacity: 0.6;
+    border: none;
+    background: transparent;
+    color: inherit;
+  }
+  
+  ${props => !props.hasDescription && css`
+    align-items: center;
+    
+    svg {
+      margin-top: 0;
+    }
+  `}
+`;
--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,34 +1,20 @@
 import React from "react";
-import {FiAlertCircle, FiXCircle} from "react-icons/fi";
 
-import { Container, Toast } from "./styles";
-import {ToastMessage, useToast} from "../../hooks/toast";
+import { Container } from "./styles";
+import {ToastMessage} from "../../hooks/toast";
+import Toast from "./Toast";
 
 interface ToastContainerProps {
     messages: ToastMessage[];
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-    const {removeToast} = useToast();
-
     return (
         <Container>
             {messages.map(message => (
                 <Toast key={message.id}
-                       hasDescription={!!message.description}
-                       type={message.type}
-                >
-                    <FiAlertCircle size={20}/>
-
-                    <div>
-                        <strong>{message.title}</strong>
-                        {message.description && <p>{message.description}</p>}
-                    </div>
-
-                    <button onClick={() => removeToast(message.id)}>
-                        <FiXCircle size={18}/>
-                    </button>
-                </Toast>
+                       toast={message}
+                />
             ))}
         </Container>
     );

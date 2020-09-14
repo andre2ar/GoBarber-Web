db76fa71bb340e1ec5e1517996dab6aa61ee5927
André Alvim Ribeiro <andre2ar@outlook.com>
2020-09-14 19:49:48 -0300

    Private routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Switch, Route } from 'react-router-dom';
+import Route from "./Route";
+import { Switch } from 'react-router-dom';
 
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import Dashboard from "../pages/Dashboard";
 
 const Routes: React.FC = () => (
     <Switch>
+        <Route path='/dashboard' isPrivate component={Dashboard}/>
+
         <Route path='/signup' component={SignUp}/>
         <Route path='/' component={SignIn}/>
     </Switch>
